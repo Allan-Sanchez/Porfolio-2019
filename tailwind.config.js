09ecc2db326ae8d35976a620593f0d3e5667463c
Allan-Sanchez <asanchezrixtun@gmail.com>
2019-06-19 18:24:31 -0600

    gridsome porfolio 2019

---
+++ tailwind.config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  theme: {
+    extend: {
+      borderWidth:{
+        '14':'14px'
+      }
+    },
+    container:{
+      padding:'1rem'
+    },
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+      blue:  '#1c75bc',
+      'blue-dark': '#064F89',
+
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c',
+      },
+    },
+    
+    fontFamily: {
+      sans: [
+        'Roboto',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      serif: [
+        'Georgia',
+        'Cambria',
+        '"Times New Roman"',
+        'Times',
+        'serif',
+      ],
+      mono: [
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
+    
+  },
+  variants: {},
+  plugins: []
+}
